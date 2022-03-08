0e9c1658add92750152063b979f1ce1e5fd2eafe
mariohamameh <mariohamameh97@gmail.com>
2022-03-08 22:46:06 +0000

    added middle.js

---
+++ middle.js
@@ -0,0 +1,45 @@
+const eqArrays=function(ar1,ar2) {
+    if(ar1.length!==ar2.length) {
+        return false;
+    }
+    for (let i=0;i<ar1.length;i++) {
+
+        if (ar1[i]!==ar2[i]) {
+
+            return false;
+        }
+    }
+    return true;
+
+}
+
+const assertArraysEqual = function(foo1,foo2) {
+
+    if (eqArrays(foo1,foo2)===false) {
+        return false;
+
+    }
+    return true;
+}
+
+const middle = function(array) {
+    let result=[];
+    if (array.length<=2) {
+        return result ;
+
+    }
+    let i =array.length/2;
+    
+    if ((array.length%2)===0) {            
+        result.push(array[i-1]);
+        result.push(array[i]); 
+ 
+        return result;      
+    }
+    else {  
+        result.push(array[Math.floor(i)]);
+        return result;
+    } 
+    
+
+}
