66902a67b2b4a8789bb40dde5f2f48df8b222483
mariohamameh <mariohamameh97@gmail.com>
2022-03-09 20:19:44 +0000

    added middle

--- middle.js
+++ middle.js
@@ -23,12 +23,12 @@ const assertArraysEqual = function(foo1,foo2) {
 }
 
 const middle = function(array) {
-    let result=[];
-    if (array.length<=2) {
+    let result = [];
+    if (array.length <= 2) {
         return result ;
 
     }
-    let i =array.length/2;
+    let i = array.length/2;
     
     if ((array.length%2)===0) {            
         result.push(array[i-1]);
@@ -40,6 +40,8 @@ const middle = function(array) {
         result.push(array[Math.floor(i)]);
         return result;
     } 
-    
-
 }
+
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+console.log(middle([1, 2, 3, 7, 4, 5, 6]));
