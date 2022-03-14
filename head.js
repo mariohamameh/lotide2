1376c89f066dcd53185b5cfc1b6e306558e7b315
mariohamameh <mariohamameh97@gmail.com>
2022-03-14 18:42:47 +0000

    added head test and assert equal test

--- head.js
+++ head.js
@@ -1,17 +1,8 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`✅✅✅ Assertion Passed: ${[actual]} equals ${[expected]}`);
-    }
-    if (actual !== expected) {
-        console.log(`🛑🛑🛑 Assertion Failed: ${[actual]} does not equal ${[expected]}`);
-    }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(foo) {
     return foo[0];
     
 }
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
