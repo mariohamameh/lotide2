fae862cbbf83e7f4c6d7ad811a7e58f63e6feae9
mariohamameh <mariohamameh97@gmail.com>
2022-03-14 22:15:19 +0000

    installed mocha and chai refactored headTest

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,5 +1,5 @@
 const assertEqual = require('../assertEqual');
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
